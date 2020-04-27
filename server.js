56936af6472eb8fb2cd60319fc618860af681285
rpharr <rpharr@richmond.edu>
2020-04-26 21:16:02 -0400

    save

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ var express = require('express');
 var app = express();
 app.get('/', function(res, req) {
   res.send('Hello world(test2)!\n');
-})
-var port = 80;
-app.listen(port);
+});
+
+var PORT = process.env.PORT || 3000;
+
+app.listen(PORT);
 console.log('Listening on port', port);
