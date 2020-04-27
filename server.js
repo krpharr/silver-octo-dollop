87e70c816da3f8fcfe955eb076585f5f2c2ed57d
rpharr <rpharr@richmond.edu>
2020-04-26 20:56:01 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var app = express();
+app.get('/', function(res, req) {
+  res.send('Hello world(test2)!\n');
+})
+var port = 80;
+app.listen(port);
+console.log('Listening on port', port);
